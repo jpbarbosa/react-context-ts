32621ec09c9bf3906e600a9bf78309037f43179a
JP Barbosa <jp@jp7.com.br>
2022-04-22 18:54:02 -0300

    Create header component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import '../styles/index.css';
 import { DataProvider } from '../contexts/dataContext';
+import Header from './Header';
 import List from './List';
 import New from './New';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header>
-        <h1>
-          <div>React Basic CRUD</div>
-          <div>With Context API And TypeScript</div>
-        </h1>
-      </header>
+      <Header />
       <DataProvider>
         <List />
         <New />
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <div className="title">React Basic CRUD</div>
+        <div className="subtitle">With Context API And TypeScript</div>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
