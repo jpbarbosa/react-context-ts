5b9166366492cd476f516247c90f5a67eaa54fda
JP Barbosa <jp@jp7.com.br>
2022-04-29 11:13:47 -0300

    Add tasks counter on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
+import { useDataContext } from '../contexts/dataContext';
+
 const Header = () => {
+  const { data } = useDataContext();
+
   return (
     <header>
       <h1>
         <div className="title">React Basic CRUD</div>
         <div className="subtitle">With Context API And TypeScript</div>
       </h1>
+      <div className="counter">{data.length}</div>
     </header>
   );
 };
